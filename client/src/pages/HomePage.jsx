58764b27653f371f3f4ccc7eddc5881c56bcf865
andrespolani <andrescamilopolaniabobadilla@gmail.com>
2024-04-06 08:40:00 -0500

    homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,35 @@
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'primereact/button';
 
 function HomePage() {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    // Simulando una carga para demostración
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+
   return (
-  <section className="bg-zinc-700 flex justify-center items-center">
+  <section className="bg-teal-300 flex justify-center items-center">
     <header className="bg-zinc-800 p-10">
       <h1 className="text-5xl py-2 font-bold">Evotes</h1>
       <p className="text-md text-slate-400">
         Easy votes secury 
       </p>
+
+
+      <Button
+          label="Get Started"
+          className="bg-zinc-500 text-white px-4 py-2 rounded-md mt-4 inline-block"
+          icon={loading ? 'pi pi-spin pi-spinner' : 'pi pi-check'}
+          loading={loading}
+          onClick={handleClick}
+        />
 
       <Link
         className="bg-zinc-500 text-white px-4 py-2 rounded-md mt-4 inline-block"
